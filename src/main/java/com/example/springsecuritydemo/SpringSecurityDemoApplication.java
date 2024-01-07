6ca464a5db54dd7bf391647083a8e52272d126cd
witchkaa <victoriasabadan1@gmail.com>
2024-01-08 00:09:25 +0200

    retrieving user's data (v2 and v3)

--- src/main/java/com/example/springsecuritydemo/SpringSecurityDemoApplication.java
+++ src/main/java/com/example/springsecuritydemo/SpringSecurityDemoApplication.java
@@ -9,6 +9,4 @@ public class SpringSecurityDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityDemoApplication.class, args);
     }
-
-
 }
--- src/main/java/com/example/springsecuritydemo/controller/HomeController.java
+++ src/main/java/com/example/springsecuritydemo/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.example.springsecuritydemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +22,17 @@ public class HomeController {
     @GetMapping("/api/greetings")
     public ResponseEntity<Map<String, String>> getGreetings() {
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON)
+                .body(Map.of("greeting", "Hello, %s!".formatted(userDetails.getUsername())));
+    }
+    @GetMapping("/api/greetings2")
+    public ResponseEntity<Map<String, String>> getGreetings2(HttpServletRequest request) {
+        UserDetails userDetails = (UserDetails) ((Authentication)request.getUserPrincipal()).getPrincipal();
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON)
+                .body(Map.of("greeting", "Hello, %s!".formatted(userDetails.getUsername())));
+    }
+    @GetMapping("/api/greetings3")
+    public ResponseEntity<Map<String, String>> getGreetings3(@AuthenticationPrincipal UserDetails userDetails) {
         return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON)
                 .body(Map.of("greeting", "Hello, %s!".formatted(userDetails.getUsername())));
     }
